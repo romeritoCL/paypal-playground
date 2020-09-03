4d50b0698df57ddcdb9ea690a5262de3021b9db5
romeritoCL <romero.latorre@gmail.com>
2020-09-03 21:23:12 +0200

    Added more tests

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller;
 
+use App\Controller\DefaultController;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
@@ -10,12 +11,30 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndexPage()
+    /**
+     * testIndex
+     */
+    public function testIndex()
     {
         $client = static::createClient();
-
-        $client->request('GET', '/');
-
+        $container = self::$container;
+        $indexRoute = $container->get('router')->generate('index');
+        $anonymousRoute = $container->get('router')->generate('anonymous-home');
+        $client->request('GET', $indexRoute);
         $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $this->assertResponseRedirects($anonymousRoute);
+    }
+
+    /**
+     * testFavicon
+     */
+    public function testFavicon()
+    {
+        $client = static::createClient();
+        $container = self::$container;
+        $faviconRoute = $container->get('router')->generate('favicon');
+        $client->request('GET', $faviconRoute);
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $this->assertResponseRedirects(DefaultController::PAYPAL_FAVICON_URL);
     }
 }
