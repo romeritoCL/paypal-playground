f0cf5609792ab2d933d9c4a8df237d52bbeca4af
romeritoCL <romero.latorre@gmail.com>
2023-02-15 12:02:59 +0100

    updated to work with FORWARD instead of DirectT

--- src/Service/Braintree/ForwardApiService.php
+++ src/Service/Braintree/ForwardApiService.php
@@ -13,7 +13,7 @@ class ForwardApiService extends AbstractBraintreeService
     /**
      * @var string
      */
-    public string $DIRECT_TOKEN_ENDPOINT = "https://forwarding.sandbox.braintreegateway.com/tsp";
+    public string $ENDPOINT = "https://forwarding.sandbox.braintreegateway.com";
 
     /**
      * @param string $paymentNonce
@@ -24,7 +24,7 @@ class ForwardApiService extends AbstractBraintreeService
     {
         try {
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $this->DIRECT_TOKEN_ENDPOINT);
+            curl_setopt($ch, CURLOPT_URL, $this->ENDPOINT);
             curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type:application/json']);
             curl_setopt(
                 $ch,
@@ -33,9 +33,52 @@ class ForwardApiService extends AbstractBraintreeService
             );
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+                'debug_transformations' => true,
+                'name' => 'default',
                 'merchant_id' => $this->gateway->config->getMerchantId(),
                 'payment_method_nonce' => $paymentNonce,
                 'device_data' => $deviceData,
+                'method' => 'POST',
+                'data' => [
+                    'order_number' => 'random_number'
+                ],
+                'url' => 'https://test.com',
+                'config' => [
+                    'name' => 'default',
+                    'url' => 'https://test.com',
+                    'methods' => ['POST'],
+                    'types' => ['NetworkTokenizedCard'],
+                    'request_format' => [
+                        'body' => 'json'
+                    ],
+                    'transformations' => [
+                        [
+                            'path' => '/body/order-number',
+                            'value' => '$order_number'
+                        ],
+                        [
+                            'path' => '/body/card-number',
+                            'value' => '$number'
+                        ],
+                        [
+                            'path' => '/body/card-cvv',
+                            'value' => '$cvv'
+                        ],
+                        [
+                            'path' => '/body/card-expiration',
+                            'value' => [
+                                'join',
+                                '/',
+                                [
+                                    'array',
+                                    '$expiration_month',
+                                    '$expiration_year'
+                                ]
+                            ]
+                        ]
+                    ]
+
+                ]
             ], true));
 
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -43,7 +86,7 @@ class ForwardApiService extends AbstractBraintreeService
             $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
             curl_close($ch);
         } catch (Exception $e) {
-            $this->logger->error('Error on ForwardAPI::'.$this->DIRECT_TOKEN_ENDPOINT.' = ' . $e->getMessage());
+            $this->logger->error('Error on ForwardAPI::'.$this->ENDPOINT.' = ' . $e->getMessage());
             return null;
         }
         return ([
