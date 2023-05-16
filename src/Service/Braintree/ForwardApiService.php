f2d26b1c37e08d7e1779992ea085beb9c3dbd878
romeritoCL <romero.latorre@gmail.com>
2023-05-16 14:54:16 +0200

    Updated Forward API example with headers

--- src/Service/Braintree/ForwardApiService.php
+++ src/Service/Braintree/ForwardApiService.php
@@ -40,7 +40,8 @@ class ForwardApiService extends AbstractBraintreeService
                 'device_data' => $deviceData,
                 'method' => 'POST',
                 'data' => [
-                    'order_number' => 'random_number'
+                    'card-id' => 'custom_card_id_1',
+                    'open-id-token' => sha1(time()),
                 ],
                 'url' => 'https://test.com',
                 'config' => [
@@ -53,28 +54,28 @@ class ForwardApiService extends AbstractBraintreeService
                     ],
                     'transformations' => [
                         [
-                            'path' => '/body/order-number',
-                            'value' => '$order_number'
-                        ],
-                        [
-                            'path' => '/body/card-number',
+                            'path' => '/body/card/pan',
                             'value' => '$number'
                         ],
                         [
-                            'path' => '/body/card-cvv',
+                            'path' => '/body/card/card-cvv',
                             'value' => '$cvv'
                         ],
                         [
-                            'path' => '/body/card-expiration',
-                            'value' => [
-                                'join',
-                                '/',
-                                [
-                                    'array',
-                                    '$expiration_month',
-                                    '$expiration_year'
-                                ]
-                            ]
+                            'path' => '/body/card/expiration-month',
+                            'value' => '$expiration_month'
+                        ],
+                        [
+                            'path' => '/body/card/expiration-year',
+                            'value' => '$expiration_year'
+                        ],
+                        [
+                            'path' => '/body/custom-fields/card-id',
+                            'value' => '$card-id'
+                        ],
+                        [
+                            'path' => '/header/Authorization',
+                            'value' => '$open-id-token'
                         ]
                     ]
 
@@ -82,15 +83,26 @@ class ForwardApiService extends AbstractBraintreeService
             ], true));
 
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_HEADERFUNCTION, function ($curl, $header) use (&$headers) {
+                $len = strlen($header);
+                $header = explode(':', $header, 2);
+                if (count($header) < 2) {
+                    return $len;
+                }
+                $headers[(trim($header[0]))] = trim($header[1]);
+                return $len;
+            });
             $result = curl_exec($ch);
             $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
             curl_close($ch);
         } catch (Exception $e) {
             $this->logger->error('Error on ForwardAPI::'.$this->ENDPOINT.' = ' . $e->getMessage());
             return null;
         }
         return ([
-            'result' => (object) json_decode($result),
+            'headers' => (object) $headers,
+            'body' => (object) json_decode($result),
             'statusCode' => $statusCode
         ]);
     }
