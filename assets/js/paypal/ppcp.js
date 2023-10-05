f198729e9faeb54791285460262996913a78adda
romeritoCL <romero.latorre@gmail.com>
2023-10-05 12:50:15 +0200

    updated branch with PPCP adcd working and 3ds not working

--- assets/js/paypal/ppcp.js
+++ assets/js/paypal/ppcp.js
@@ -1,64 +1,17 @@
-import JSONEditor from 'jsoneditor';
-import 'jsoneditor/dist/jsoneditor.css';
-import paypalPayments from './payments';
-window.JSONEditor = JSONEditor;
-
+let orderCaptureUrl = document.getElementById('order-capture-url').dataset.orderCaptureUrl;
+let orderCreateUrl = document.getElementById('order-create-url').dataset.orderCreateUrl;
 let settings = JSON.parse(document.getElementById('customer-settings').dataset.settings);
 let orderCreateJson = {
-    intent: 'capture',
-    payer: {
-        name: {
-            given_name: settings['settings-customer-name'],
-            surname: settings['settings-customer-family-name'],
-        },
-        email_address: settings['settings-customer-email'],
-        address: {
-            address_line_1: settings['settings-customer-street'],
-            country_code: settings['settings-customer-country'],
-            admin_area_1: settings['settings-customer-province'],
-            admin_area_2: settings['settings-customer-city'],
-            postal_code: settings['settings-customer-zip-code']
-        }
-    },
-    purchase_units: [{
-        reference_id: settings['settings-item-sku'],
-        amount: {
-            currency_code: settings['settings-customer-currency'],
-            value: (+settings['settings-item-price'] + +settings['settings-item-shipping']),
-            breakdown: {
-                item_total: {
-                    value: settings['settings-item-price'],
-                    currency_code: settings['settings-customer-currency']
-                },
-                shipping: {
-                    value: (+settings['settings-item-shipping'] * 2),
-                    currency_code: settings['settings-customer-currency']
-                },
-                shipping_discount: {
-                    value: settings['settings-item-shipping'],
-                    currency_code: settings['settings-customer-currency']
-                }
+    "intent": "CAPTURE",
+    "purchase_units": [
+        {
+            "amount": {
+                "currency_code": "GBP",
+                "value": "50"
             }
-        },
-        description: settings['settings-item-description'],
-        items: [{
-            name: settings['settings-item-name'],
-            unit_amount: { currency_code: settings['settings-customer-currency'], value: settings['settings-item-price']},
-            quantity: 1,
-            sku: settings['settings-item-sku'],
-            description: settings['settings-item-description']
-        }],
-    }]
+        }
+    ]
 };
-
-let paypalButtonsStyle = {
-    layout: 'vertical',
-    color: 'gold',
-    shape: 'pill',
-    label: 'pay'
-};
-
-paypalPayments.startPayments(orderCreateJson, paypalButtonsStyle);
 
 if (paypal.HostedFields.isEligible()) {
     let orderId
@@ -66,7 +19,15 @@ if (paypal.HostedFields.isEligible()) {
     paypal.HostedFields.render({
         // Call your server to set up the transaction
         createOrder: function (data, actions) {
-            return actions.order.create(orderCreateEditor.get());
+            return fetch(orderCreateUrl, {
+                method: "post",
+                body: JSON.stringify(orderCreateJson)
+            })
+            .then((res) => res.json())
+            .then((orderData) => {
+                orderId = orderData.id
+                return orderData.id
+            })
         },
         styles: {
             '.valid': {
@@ -75,6 +36,17 @@ if (paypal.HostedFields.isEligible()) {
             '.invalid': {
                 color: 'red',
             },
+            'input': {
+                'font-size': '16px',
+                'font-family': 'sans-serif',
+                color: '#3A3A3A',
+                transition: 'color 160ms linear',
+                '-webkit-transition': 'color 160ms linear',
+                '-webkit-font-smoothing': 'antialiased'
+            },
+            ':focus': {
+                color: '#333333'
+            }
         },
         fields: {
             number: {
@@ -92,38 +64,28 @@ if (paypal.HostedFields.isEligible()) {
         },
     }).then((cardFields) => {
         document.querySelector('#paypal-hosted-fields').addEventListener('submit', (event) => {
-            event.preventDefault()
+            event.preventDefault();
             cardFields
                 .submit({
-                    cardholderName: document.getElementById('card-holder-name').value
+                    cardholderName: document.getElementById('card-holder-name').value,
+                    contingencies: ['SCA_ALWAYS'] //SCA_WHEN_REQUIRED
                 })
-                .then(() => {
-                    fetch(`/api/orders/${orderId}/capture`, {
+                .then(function (payload) {
+                    console.log(payload)
+                    orderCaptureUrl = orderCaptureUrl.replace("payment_id_status", orderId)
+                    fetch(orderCaptureUrl, {
                         method: 'post',
+
                     })
-                        .then((res) => res.json())
-                        .then((orderData) => {
-                            // Two cases to handle:
-                            //   (1) Other non-recoverable errors -> Show a failure message
-                            //   (2) Successful transaction -> Show confirmation or thank you
-                            // This example reads a v2/checkout/orders capture response, propagated from the server
-                            // You could use a different API or structure for your 'orderData'
-                            const errorDetail =
-                                Array.isArray(orderData.details) && orderData.details[0]
-                            if (errorDetail) {
-                                var msg = 'Sorry, your transaction could not be processed.'
-                                if (errorDetail.description)
-                                    msg += '\n\n' + errorDetail.description
-                                if (orderData.debug_id)
-                                    msg += ' (' + orderData.debug_id + ')'
-                                return alert(msg) // Show a failure message
-                            }
-                            // Show a success message or redirect
-                            alert('Transaction completed!')
-                        })
+                    .then((response) => {
+                        return response.text()
+                    })
+                    .then((html) => {
+                        document.getElementById('result-col').innerHTML = html
+                    })
                 })
                 .catch((err) => {
-                    alert('Payment could not be captured! ' + JSON.stringify(err))
+                    document.getElementById('result-col').innerHTML = "<pre>" + JSON.stringify(err, null ,2) + "</pre>"
                 })
         })
     })
